8228f81d837fbd5373a3e1d33b07fdff84b03d37
8lovelife <dmzlovelife@gmail.com>
2022-09-17 15:52:57 +0800

    add two numbers

---
+++ src/main/java/com/leetcode/easy/AddTwoNumbers.java
@@ -0,0 +1,45 @@
+package com.leetcode.easy;
+
+import lombok.Data;
+
+public class AddTwoNumbers {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int l1Var = l1 != null ? l1.val : 0;
+            int l2Var = l2 != null ? l2.val: 0;
+            int sum = l1Var + l2Var + carry;
+            carry = sum / 10; // 取整
+            int remainder = sum % 10; // 取余
+            curr.next = new ListNode(remainder);
+            curr = curr.next;
+            if (l1!= null) {
+                l1 = l1.next;
+            }
+            if (l2!= null) {
+                l2 = l2.next;
+            }
+        }
+        return dummyHead.next;
+    }
+}
+
+@Data
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
--- src/test/java/com/leetcode/easy/EasyTest.java
+++ src/test/java/com/leetcode/easy/EasyTest.java
@@ -2,19 +2,40 @@ package com.leetcode.easy;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 /**
  * Unit test for simple App.
  */
-public class EasyTest 
-{
+public class EasyTest {
 
     @Test
-    public void twoSum()
-    {
-        int[] nums = new int[]{1,3,7,5,4};
+    public void twoSum() {
+        int[] nums = new int[] { 1, 3, 7, 5, 4 };
         int target = 4;
         int[] result = TwoSum.twoSum(nums, target);
-        Assert.assertTrue(result!=null 
-        && (nums[result[0]] + nums[result[1]]) == target); 
+        Assert.assertTrue(result != null
+                && (nums[result[0]] + nums[result[1]]) == target);
+    }
+
+    @Test
+    public void addTwoNumbers() {
+        int[] l1 = new int[] { };
+        int[] l2 = new int[] { 0,1};
+        ListNode l1DummyHead = new ListNode();
+        ListNode currL1 = l1DummyHead;
+        for (int i = 0; i < l1.length; i++) {
+            currL1.next = new ListNode(l1[i]);
+            currL1 = currL1.next;
+        }
+        ListNode l2DummyHead = new ListNode();
+        ListNode curryL2 = l2DummyHead;
+        for (int j = 0; j < l2.length; j++) {
+            curryL2.next = new ListNode(l2[j]);
+            curryL2 = curryL2.next;
+        }
+        System.out.println(l1DummyHead.next);
+        System.out.println(l2DummyHead.next);
+        ListNode l3 = AddTwoNumbers.addTwoNumbers(l1DummyHead.next,l2DummyHead.next);
+        System.out.println(l3);
     }
 }
