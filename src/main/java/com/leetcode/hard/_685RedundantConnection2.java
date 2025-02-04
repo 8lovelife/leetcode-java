de243cf5f0b8e85ce79a97e7622599d5012ee123
Mark Yin <dmzlovelife@gmail.com>
2025-02-04 01:52:07 +0000

    disjoint set

---
+++ src/main/java/com/leetcode/hard/_685RedundantConnection2.java
@@ -0,0 +1,86 @@
+package com.leetcode.hard;
+
+import java.util.Arrays;
+
+public class _685RedundantConnection2 {
+
+    public static void main(String[] args) {
+        int[][] edges = new int[][]{{2,1},{3,1},{4,3},{1,4}};
+        int[] result = new _685RedundantConnection2().findRedundantConnection(edges);
+        System.out.println(Arrays.toString(result));
+
+    }
+    // AC: runtime 96.65% mem 88.66%
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        int[] roots = new int[n + 1];
+        DisjointSet dSet = new DisjointSet(n + 1);
+        for (int i = 1; i <= n; i++) {
+            roots[i] = i;
+        }
+
+        int[] leadTwoRoots = new int[0];
+        int[] leadCycle = new int[0];
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            // two roots check
+            if (roots[v] != v) {
+                leadTwoRoots = edge;
+                continue;
+            }
+            roots[v] = u;
+
+            // cycle check
+            if (dSet.isConnected(u, v)) {
+                leadCycle = edge;
+            }
+            dSet.union(u, v);
+        }
+
+        if (leadTwoRoots.length == 0) {
+            return leadCycle;
+        }
+
+        if (leadCycle.length == 0) {
+            return leadTwoRoots;
+        }
+
+        int midNode = leadTwoRoots[1];
+        return new int[]{roots[midNode],midNode};
+
+}
+
+class DisjointSet {
+    private int[] parent;
+
+    DisjointSet(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findRoot(int v) {
+        if (parent[v] == v) {
+            return v;
+        } 
+        parent[v] = findRoot(parent[v]);
+        return parent[v];
+    }
+
+    void union(int u,int v){
+        int uRoot = findRoot(u);
+        int vRoot = findRoot(v);
+        if (vRoot!=uRoot) {
+            parent[uRoot] = vRoot;
+        }
+    }
+
+    boolean isConnected(int u,int v){
+        return findRoot(u) == findRoot(v);
+    }
+
+}
--- src/main/java/com/leetcode/medium/_684RedundantConnection.java
+++ src/main/java/com/leetcode/medium/_684RedundantConnection.java
@@ -1,8 +1,60 @@
 package com.leetcode.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _684RedundantConnection {
 
-    // AC: runtime 74.40% mem 70.72%
+    public static void main(String[] args) {
+        int[][] edges = new int[][] {{1,2},{1,3},{2,3}};
+
+        new _684RedundantConnection().findRedundantConnectionDFS(edges);
+    }
+
+    // AC: runtime 28.85% mem 20.11%
+    public int[] findRedundantConnectionDFS(int[][] edges) {
+        int n = edges.length;
+        List<Integer>[] graph = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        boolean[] visited = new boolean[n + 1];
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            if (hasPath(u,v,visited,graph)) {
+                return edge;
+            }
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+        return new int[0];
+    }
+
+    public boolean hasPath(int u,int v, boolean[] visited,List<Integer>[] graph) {
+        if (u == v) {
+            return true;
+        }
+        if (visited[u]) {
+            return false;
+        }
+
+        visited[u] = true;
+        for (int neighbor : graph[u]) {
+            // a little optimization
+            // if (graph[u].isEmpty() || graph[v].isEmpty()) {
+            //     continue;
+            // }
+
+            if(hasPath(neighbor, v, visited, graph)){
+                return true;
+            }
+        }
+        visited[u] = false;
+        return false;
+    }
+
+    // AC: runtime 79.95% mem 99.98%
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
         UnionFind unionFind = new UnionFind(n);
@@ -33,15 +85,24 @@ class UnionFind {
         return find(parent[a]);
     }
 
+    public int findWithPathCompression(int a){
+        if (parent[a] == a) {
+            return a;
+        } else {
+            parent[a] = findWithPathCompression(parent[a]);
+            return parent[a];
+        }
+    }
+
     public void union(int a, int b){
-        int rootA = find(a);
-        int rootB = find(b);
+        int rootA = findWithPathCompression(a);
+        int rootB = findWithPathCompression(b);
         if (rootA != rootB) {
             parent[rootA] = rootB;
         }
     }
 
     public Boolean isConnected(int a,int b){
-        return find(a) == find(b);
+        return findWithPathCompression(a) == findWithPathCompression(b);
     }
 }
