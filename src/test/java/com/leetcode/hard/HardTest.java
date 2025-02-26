69c82131a5fd0f594f87ece52080713c43dbf844
dmzlovelife <dmzlovelife@gmail.com>
2025-02-26 20:33:05 +0000

    .fold update

--- src/test/java/com/leetcode/hard/HardTest.java
+++ src/test/java/com/leetcode/hard/HardTest.java
@@ -1,11 +1,9 @@
 package com.leetcode.hard;
 
-import com.leetcode.hard._2493DivideNodesIntoMaxGroups;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class HardTest {
-
 
     @Test
     public void _2493() {
@@ -16,16 +14,16 @@ public class HardTest {
 
     @Test
     public void _2608() {
-        int[][] edges = new int[][] { { 4,1 },{3,2},{5,0},{3,0},{4,0},{2,1},{5,1} };
+        int[][] edges = new int[][] { { 4, 1 }, { 3, 2 }, { 5, 0 }, { 3, 0 }, { 4, 0 }, { 2, 1 }, { 5, 1 } };
         int shortestCycle = new _2608ShortestCycle().findShortestCycleDFS(6, edges);
         Assert.assertTrue(shortestCycle == 4);
     }
 
     @Test
     public void _2360() {
-        int[] edges = new int[] { 1,0 };
+        int[] edges = new int[] { 1, 0 };
         int shortestCycle = new _2360LongestCycle().longestCycle(edges);
         Assert.assertTrue(shortestCycle == 2);
     }
-    
+
 }
