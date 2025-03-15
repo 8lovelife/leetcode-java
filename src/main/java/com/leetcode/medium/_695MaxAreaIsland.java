c3380649acf8f32fb8ff1f3d61bfb1d9e2c25305
8lovelife <dmzlovelife@gmail.com>
2025-03-15 15:11:54 -0700

    implementation of leetcode 695

--- src/main/java/com/leetcode/medium/_695MaxAreaIsland.java
+++ src/main/java/com/leetcode/medium/_695MaxAreaIsland.java
@@ -29,4 +29,31 @@ public class _695MaxAreaIsland {
         dfs(grid, r, rL, c - 1, cL, area);
         dfs(grid, r, rL, c + 1, cL, area);
     }
+
+    public int maxAreaOfIslandWithReturn(int[][] grid) {
+        int rL = grid.length;
+        int cL = grid[0].length;
+        int maxArea = 0;
+        for (int r = 0; r < rL; r++) {
+            for (int c = 0; c < cL; c++) {
+                if (grid[r][c] == 1) {
+                    maxArea = Math.max(dfsWithReturn(grid, r, rL, c, cL), maxArea);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private int dfsWithReturn(int[][] grid, int r, int rL, int c, int cL) {
+        if (r >= rL || r < 0 || c >= cL || c < 0 || grid[r][c] == 0) {
+            return 0;
+        }
+        grid[r][c] = 0;
+        int count = 1;
+        count += dfsWithReturn(grid, r - 1, rL, c, cL);
+        count += dfsWithReturn(grid, r + 1, rL, c, cL);
+        count += dfsWithReturn(grid, r, rL, c - 1, cL);
+        count += dfsWithReturn(grid, r, rL, c + 1, cL);
+        return count;
+    }
 }
