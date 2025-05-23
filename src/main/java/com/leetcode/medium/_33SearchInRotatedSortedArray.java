315460bcd262292d943e61454904667a3374cfb4
8lovelife <dmzlovelife@gmail.com>
2025-05-23 12:01:28 -0700

    33 leetcode

---
+++ src/main/java/com/leetcode/medium/_33SearchInRotatedSortedArray.java
@@ -0,0 +1,36 @@
+package com.leetcode.medium;
+
+public class _33SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midNum = nums[mid];
+
+            if (midNum == target) {
+                return mid;
+            }
+
+            if (midNum <= nums[right]) {
+
+                if (target <= nums[right] && target >= midNum) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+
+            } else {
+                if (target >= nums[left] && target <= midNum) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            }
+
+        }
+
+        return -1;
+    }
+}
