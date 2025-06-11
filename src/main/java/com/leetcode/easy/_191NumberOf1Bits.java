3d66e6df9ed15b64eea9868acb2abfb50e13fc6b
8lovelife <dmzlovelife@gmail.com>
2025-06-10 22:00:47 -0700

    hamming weight

--- src/main/java/com/leetcode/easy/_191NumberOf1Bits.java
+++ src/main/java/com/leetcode/easy/_191NumberOf1Bits.java
@@ -4,11 +4,20 @@ public class _191NumberOf1Bits {
     public int hammingWeight(int n) {
         int hammingWeight = 0;
         while (n > 0) {
-            int bit = n & 1;
+            int bit = n & 1; // check the last bit
             if (bit == 1) {
                 hammingWeight++;
             }
             n >>= 1;
+        }
+        return hammingWeight;
+    }
+
+    public int hammingWeight2(int n) {
+        int hammingWeight = 0;
+        while (n > 0) {
+            hammingWeight++;
+            n &= n - 1; // remove the last bit which is '1'
         }
         return hammingWeight;
     }
