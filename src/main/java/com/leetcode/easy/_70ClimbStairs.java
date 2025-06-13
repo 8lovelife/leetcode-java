7f951e67ac9e324803c29a7860e2ea7196e806c7
8lovelife <dmzlovelife@gmail.com>
2025-06-12 22:28:12 -0700

    space o(1) for climb stairs

--- src/main/java/com/leetcode/easy/_70ClimbStairs.java
+++ src/main/java/com/leetcode/easy/_70ClimbStairs.java
@@ -12,4 +12,18 @@ public class _70ClimbStairs {
         }
         return dp[n];
     }
+
+    public int climbingStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int a = 1;
+        int b = 1;
+        for (int i = 2; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
 }
