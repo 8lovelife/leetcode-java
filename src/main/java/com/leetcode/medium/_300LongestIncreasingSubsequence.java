34bb0470d6265b0264457019b23786427eb67111
8lovelife <dmzlovelife@gmail.com>
2025-06-22 23:55:31 -0700

    optimize longest increasing subsequence

--- src/main/java/com/leetcode/medium/_300LongestIncreasingSubsequence.java
+++ src/main/java/com/leetcode/medium/_300LongestIncreasingSubsequence.java
@@ -3,6 +3,25 @@ package com.leetcode.medium;
 import java.util.Arrays;
 
 public class _300LongestIncreasingSubsequence {
+
+    public int lengthOfLIS2(int[] nums) {
+        int n = nums.length;
+        int[] tails = new int[n];
+        int size = 0;
+        for (int num : nums) {
+            int index = Arrays.binarySearch(tails, 0, size, num);
+            if (index < 0) {
+                index = -index - 1;
+            }
+            tails[index] = num;
+            if (index == size) {
+                size++;
+            }
+        }
+        return size;
+    }
+
+    // n squared
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
