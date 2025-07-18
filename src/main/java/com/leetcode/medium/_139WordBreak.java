5523cf24234009fd0f49fec9bcc3f46263fb62b2
8lovelife <dmzlovelife@gmail.com>
2025-07-18 14:19:27 -0700

    word break 2

--- src/main/java/com/leetcode/medium/_139WordBreak.java
+++ src/main/java/com/leetcode/medium/_139WordBreak.java
@@ -1,6 +1,7 @@
 package com.leetcode.medium;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
@@ -28,9 +29,9 @@ public class _139WordBreak {
         return false;
     }
 
-    // BFS with optimization
+    // BFS with optimization ( n * n * n + m * k)
     public boolean wordBreakOptimization(String s, List<String> wordDict) {
-        Set<String> words = new HashSet<>(wordDict);
+        Set<String> words = new HashSet<>(wordDict); // m * k
         Queue<Integer> q = new ArrayDeque<>();
         boolean[] visited = new boolean[s.length() + 1];
         q.offer(0);
@@ -51,6 +52,73 @@ public class _139WordBreak {
             visited[start] = true;
         }
         return false;
+    }
+
+    // n * m * k
+    public boolean wordBreakTopDownDFS(String s, List<String> wordDict) {
+        int[] mem = new int[s.length()];
+        Arrays.fill(mem, -1);
+        return dp(s.length() - 1, mem, wordDict, s); // n is the state of the s indexs
+    }
+
+    private boolean dp(int endIndex, int[] mem, List<String> wordDict, String s) {
+        if (endIndex < 0) {
+            return true;
+        }
+        if (mem[endIndex] != -1) {
+            return mem[endIndex] == 1;
+        }
+        for (String word : wordDict) { // m is the length of wordDict
+            int startIndex = endIndex - word.length() + 1;
+            if (startIndex < 0) {
+                continue;
+            }
+
+            if (s.substring(startIndex, endIndex + 1).equals(word) // k is the length of s
+                    && dp(startIndex - 1, mem, wordDict, s)) {
+                mem[endIndex] = 1;
+                return true;
+            }
+        }
+        mem[endIndex] = 0;
+        return false;
+    }
+
+    // n * m * k
+    public boolean wordBreakBottomUpDP(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            for (String word : wordDict) {
+                int wordL = word.length();
+                if (i < wordL - 1) {
+                    continue;
+                }
+                if (i == wordL - 1 || dp[i - wordL]) {
+                    if (s.substring(i - wordL + 1, i + 1).equals(word)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+
+    public boolean wordBreakBottomUpDp2(String s, List<String> wordDict) {
+        int n = s.length();
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && words.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 
 }
