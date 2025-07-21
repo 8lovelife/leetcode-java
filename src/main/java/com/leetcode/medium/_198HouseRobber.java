529b625ce4562bf04c24a1c6d82c3bac507a5e8f
8lovelife <dmzlovelife@gmail.com>
2025-07-21 16:06:24 -0700

    leetcode 198

---
+++ src/main/java/com/leetcode/medium/_198HouseRobber.java
@@ -0,0 +1,56 @@
+package com.leetcode.medium;
+
+import java.util.Arrays;
+
+public class _198HouseRobber {
+
+    // two to the power of n
+    public int rob(int[] nums) {
+        return dfs(nums, 0);
+    }
+
+    private int dfs(int[] nums, int curIndex) {
+        if (curIndex >= nums.length) {
+            return 0;
+        }
+        return Math.max(dfs(nums, curIndex + 1),
+                dfs(nums, curIndex + 2) + nums[curIndex]);
+    }
+
+    // top-down O(N)
+
+    public int robWithMemory(int[] nums) {
+        int[] robbed = new int[nums.length];
+        Arrays.fill(robbed, -1);
+        return dfsWithMemory(nums, 0, robbed);
+    }
+
+    private int dfsWithMemory(int[] nums, int curIndex, int[] robbed) {
+        if (curIndex >= nums.length) {
+            return 0;
+        }
+        if (robbed[curIndex] != -1) {
+            return robbed[curIndex];
+        }
+        int totalAmount = Math.max(dfsWithMemory(nums, curIndex + 1, robbed),
+                dfsWithMemory(nums, curIndex + 2, robbed) + nums[curIndex]);
+        robbed[curIndex] = totalAmount;
+        return totalAmount;
+    }
+
+    // bottom up O(N)
+    public int robBottomUp(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+
+}
--- src/main/java/com/leetcode/medium/_39CombinationSum.java
+++ src/main/java/com/leetcode/medium/_39CombinationSum.java
@@ -5,6 +5,32 @@ import java.util.Arrays;
 import java.util.List;
 
 public class _39CombinationSum {
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs2(candidates, 0, 0, target, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void dfs2(int[] candidates, int curIndex, int curTarget, int target, List<Integer> result,
+            List<List<Integer>> ans) {
+        if (target == curTarget) {
+            ans.add(new ArrayList<>(result));
+            return;
+        } else if (target < curTarget) {
+            return;
+        }
+        for (int i = curIndex; i < candidates.length; i++) {
+            int curCandidate = candidates[i];
+            if (curCandidate > target) {
+                continue;
+            }
+            result.add(curCandidate);
+            dfs(candidates, i, curTarget + curCandidate, target, result, ans);
+            result.remove(result.size() - 1);
+        }
+    }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> result = new ArrayList<>();
