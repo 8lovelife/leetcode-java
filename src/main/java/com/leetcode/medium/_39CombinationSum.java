e456ab8dcb9aeef925256ffc0e15c300cc74ebb5
8lovelife <dmzlovelife@gmail.com>
2025-07-20 21:37:19 -0700

    leetcode 39

---
+++ src/main/java/com/leetcode/medium/_39CombinationSum.java
@@ -0,0 +1,34 @@
+package com.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _39CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfs(candidates, 0, 0, target, result, ans);
+        return ans;
+    }
+
+    private void dfs(int[] candidates, int curIndex, int curTarget, int target, List<Integer> result,
+            List<List<Integer>> ans) {
+        if (target == curTarget) {
+            ans.add(new ArrayList<>(result));
+            return;
+        } else if (target < curTarget) {
+            return;
+        }
+        for (int i = curIndex; i < candidates.length; i++) {
+            int curCandidate = candidates[i];
+            if (curCandidate > target) {
+                return;
+            }
+            result.add(curCandidate);
+            dfs(candidates, i, curTarget + curCandidate, target, result, ans);
+            result.remove(result.size() - 1);
+        }
+    }
+}
